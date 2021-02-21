b547287a15636a01a6f5ae5fd5e94f3cb0c1e936
Danaproject <decoratio@gmail.com>
2021-02-21 23:57:30 +0100

    +styles

--- src/createList.js
+++ src/createList.js
@@ -3,7 +3,7 @@ import refs from './refs';
 function createList(countries) {
     let listItems = [];
     countries.map(item => {
-        listItems.push(`<ul><li>${item.name}</li></ul>`)
+        listItems.push(`<ul class="suggestions"><li>${item.name}</li></ul>`)
      });
     refs.countriesContainer.innerHTML = '';
     refs.countriesContainer.insertAdjacentHTML('beforeend', listItems.join(''));
