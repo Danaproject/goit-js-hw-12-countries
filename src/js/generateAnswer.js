d9d8767c4bb20b4b285b4c5048f62df336ebf156
Danaproject <decoratio@gmail.com>
2021-02-22 13:18:46 +0100

    organized js

--- src/js/generateAnswer.js
+++ src/js/generateAnswer.js
@@ -3,12 +3,11 @@ import createList from './createList';
 import notify from './notify';
 
 function generateAnswer(countries) {
-  if (countries.status !== 404) {
+    if (countries.status === 404) return;
     const number = countries.length;
     if (number === 1) createMarkup(countries);
     if (number >= 2 & number <= 10) createList(countries);
     if (number > 10) notify();
-  }  
 }
 
 export default generateAnswer;
